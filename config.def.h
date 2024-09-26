d5f6e82642929a0340140ed16d291a999ad53243
ki11errabbit <ki11errabbit@posteo.net>
2024-09-25 21:27:34 -0600

    removed dwl from dbus activation thing

--- config.def.h
+++ config.def.h
@@ -39,7 +39,7 @@ static const char *const autostart[] = {
         "nm-applet", NULL,
         "blueman-applet", NULL,
         "kdeconnect-indicator", NULL,
-        "dbus-update-activation-environment", "--systemd", "WAYLAND_DISPLAY", "XDG_CURRENT_DESKTOP=dwl", NULL,
+        "dbus-update-activation-environment", "--systemd", "WAYLAND_DISPLAY", "XDG_CURRENT_DESKTOP", NULL,
         NULL /* terminate */
 };
 
